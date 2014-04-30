84a3d0a11fab2982f92f149d9827a1af1456765f
Derrick Showers <derrick@derrickshowers.com>
2014-04-30 16:42:30 -0700

    moving all markup to JS for better control

--- js/main.js
+++ js/main.js
@@ -1,19 +1,56 @@
-(function() {
+var templates = {
+	login: '\
+	<form id="login">\
+		<div class="form-group">\
+			<label for="loUsername">Username</label>\
+			<input type="text" class="form-control" id="loUsername" placeholder="">\
+		</div>\
+		<div class="form-group">\
+			<label for="loPassword">Password</label>\
+			<input type="password" class="form-control" id="loPassword" placeholder="">\
+		</div>\
+		<button type="submit" class="btn btn-primary">Login</button>\
+	</form>\
+	',
+	signup: '\
+	<form id="signup">\
+		<div class="form-group">\
+			<label for="suUsername">Username</label>\
+			<input type="text" class="form-control" id="suUsername" placeholder="Pick a username">\
+		</div>\
+		<div class="form-group">\
+			<label for="suPassword">Password</label>\
+			<input type="password" class="form-control" id="suPassword" placeholder="Make it secure!">\
+		</div>\
+		<div class="form-group">\
+			<label for="suEmail">Email Address</label>\
+			<input type="email" class="form-control" id="suEmail" placeholder="ex. me@me.com">\
+		</div>\
+		<div class="form-group">\
+			<label for="suPhone">Phone Number</label>\
+			<input type="text" class="form-control" id="suPhone" placeholder="ex. (123) 456-7890">\
+		</div>\
+		<button type="submit" class="btn btn-primary">Signup</button>\
+	</form>\
+	'
+};
+
+;(function() {
 	'use strict';
 
-	var $data = $('#data');
+	var $view = $('#view');
 	var loggedIn = false;
-	Parse.initialize("JQmm2hKsFD7OJoVzBzJl4uUaV6VABCU059u9ZHBN", "oDmb5bkVH33KnVbNmqAK45uAd5VQ4xSbvFDAoe0T");
+	Parse.initialize('JQmm2hKsFD7OJoVzBzJl4uUaV6VABCU059u9ZHBN', 'oDmb5bkVH33KnVbNmqAK45uAd5VQ4xSbvFDAoe0T');
 
 	var login = function(user) {
 		loggedIn = true;
-		$(data).html('<h3>Great! You\'re logged in.</h3>');
+		$view.html('<h3>Great! You\'re logged in.</h3>');
 		$('#showLogin').text('Logout');
 		$('#signup').hide();
 
 		// display info
 		var info = '<p>Name: ' + user.get('username') + '</p><p>Email: ' + user.get('email') + '</p><p>Phone: ' + user.get('phone') + '</p>';
-		$(data).html($(data).html() + info);
+		$view.html($view.html() + info);
 	}
 
 	var storeUser = function(username, password, email, phone) {
@@ -30,7 +67,7 @@
 			},
 			error: function(user, error) {
 				// Show the error message somewhere and let the user try again.
-				$(data).text('Error: ' + error.code + ' ' + error.message);
+				$view.text('Error: ' + error.code + ' ' + error.message);
 			}
 		});
 
@@ -43,7 +80,7 @@
 				login(user);
 			},
 			error: function(user, error) {
-				$(data).text('Error: ' + error.code + ' ' + error.message);
+				$view.text('Error: ' + error.code + ' ' + error.message);
 			}
 		});
 
@@ -51,31 +88,28 @@
 
 	var init = function() {
 
-		$('#signup').hide();
-		$('#login').hide();
-
 		// event listenres
 		$('#showSignup').click(function() {
-			$(data).text('');
-			$('#login').hide();
-			$('#signup').show();
+			$.get('https://api.parse.com', function(data) {
+				console.log(data);
+			});
+			$view.html(templates.signup);
+				$('#signup').submit(function(e) {
+				e.preventDefault();
+				storeUser($('#suUsername').val(), $('#suPassword').val(), $('#suEmail').val(), $('#suPhone').val())
+			});
 		});
 		$('#showLogin').click(function() {
 			if ($(this).text() === 'Login') {
-				$(data).text('');
-				$('#signup').hide();
-				$('#login').show();
+				$view.html(templates.login);
+				$('#login').submit(function(e) {
+					e.preventDefault();
+					getUser($('#loUsername').val(), $('#loPassword').val());
+				});
 			} else {
+				$view.text('You\'ve been logged out');
 				$(this).text('Login');
 			}
-		});
-		$('#signup').submit(function(e) {
-			e.preventDefault();
-			storeUser($('#suUsername').val(), $('#suPassword').val(), $('#suEmail').val(), $('#suPhone').val())
-		});
-		$('#login').submit(function(e) {
-			e.preventDefault();
-			getUser($('#loUsername').val(), $('#loPassword').val());
 		});
 	}
 
